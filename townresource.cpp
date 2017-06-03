367f3dc767ce06516eab8880916914011b6fd26c
Brendan Hosking <brendan.hosking@gmail.com>
2017-06-03 23:47:05 +1000

    Add basic functions for TownResource
    TownResource has public variables and no implementation details.
    
    Add getter and setter methods for all variables that need them.
    Implement method by which the buy and sell price is calculated.
    This calculation is the sum of all individual transactions as if the
    selling/buying was occurring one by one. The individual in price is
    basePrice * 2^(-decay * stock) and the out price is the same but
    stock -> stock-1. Renamed buyPrice and sellPrice to outPrice and inPrice
    respectively to reduce confusion.

--- townresource.cpp
+++ townresource.cpp
@@ -1,22 +1,62 @@
 #include "townresource.h"
 #include <math.h>
 
-TownResource::TownResource()
+TownResource::TownResource(const Resource *resource, float rate, float stock)
+    :m_resource(resource), m_rate(rate), m_stock(stock)
 {
 
 }
 
-float TownResource::buyPrice(int num) const
+const Resource *TownResource::getResource() const
 {
-
+    return m_resource;
 }
 
-float TownResource::sellPrice(int num) const
+float TownResource::getRate() const
 {
+    return m_rate;
+}
 
+float TownResource::getStock() const
+{
+    return m_stock;
+}
+
+float TownResource::outPrice(int num) const
+{
+    return getBulkValue(getStock() - 1, -1 * num);
+}
+
+float TownResource::inPrice(int num) const
+{
+    return getBulkValue(getStock(), num);
 }
 
 float TownResource::getBulkValue(int startStock, int deltaStock) const
 {
+    decay = -1 * getResource()->getDecay();
+    return exp2(decay * startStock) * (1 - exp2(deltaStock * decay)) / (1 - exp2(decay));
+}
 
+std::vector<std::pair<TownResource *, float> > &TownResource::getTownResourcesNeeded() const
+{
+    return m_townResourcesNeeded;
+}
+
+void TownResource::setStock(float newStock)
+{
+    m_stock = newStock;
+}
+
+void TownResource::setTownResourcesNeeded(std::vector<TownResource *> townResources)
+{
+    for (std::pair<const Resource *, float> need:getResource()->getNeeds())
+    {
+        for (TownResource * townResource:townResources) {
+            if (townResource->getResource() == need[0]) {
+                m_townResourcesNeeded.push_back(std::pair<TownResource *, float> (townResource, need[1]));
+                break;
+            }
+        }
+    }
 }
--- townresource.h
+++ townresource.h
@@ -5,14 +5,22 @@
 class TownResource
 {
 public:
-    TownResource();
-    const Resource * resource;
-    float rate;
-    float stock;
-    float buyPrice(int num = 1) const;
-    float sellPrice(int num = 1) const;
+    TownResource(const Resource * resource, float rate, float stock = 0);
+    const Resource * getResource() const;
+    float getRate() const;
+    float getStock() const;
+    float outPrice(int num = 1) const;
+    float inPrice(int num = 1) const;
     float getBulkValue(int startStock, int deltaStock = 1) const;
-    std::vector<std::pair<TownResource *,float> > townResourcesNeeded;
+    std::vector<std::pair<TownResource *,float> > & getTownResourcesNeeded() const;
+    void setStock(float newStock);
+    void setTownResourcesNeeded(std::vector<TownResource *> neededResources);
+
+private:
+    const Resource * m_resource;
+    float m_rate;
+    float m_stock;
+    std::vector<std::pair<TownResource *,float> > m_townResourcesNeeded;
 };
 
 #endif // TOWNRESOURCE_H
