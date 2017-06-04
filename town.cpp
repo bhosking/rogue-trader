dbcb7448fd726239dac1e97d1ae515fccea8be25
Brendan Hosking <brendan.hosking@gmail.com>
2017-06-04 11:15:37 +1000

    Add Basic getter functions to Town
    Town class lacks variables and implementation details for the functions
    it does have.
    
    Introduce population, name, pos and resources variables, and write
    getter functions for these. Partially write constructor function
    (still need to initialise resource requirements).

--- town.cpp
+++ town.cpp
@@ -1,10 +1,33 @@
 #include "town.h"
-Town::Town()
+Town::Town(std::vector<std::tuple<const Resource *, float, float> > resourceRatesStock, unsigned population, std::string name, QPointF pos)
+    :m_population(population), m_name(name), m_pos(pos)
 {
-
+    for (std::tuple<const Resource *, float, float> resouceInits:resourceRatesStock) {
+        m_resources.push_back(new TownResource(std::get<0>(resourceInits), std::get<1>(resourceInits), std::get<2>(resouceInits)));
+    }
 }
 
-void Town::processTick(World * world)
+std::vector<TownResource *> Town::getResources() const
+{
+    return m_resources;
+}
+
+const unsigned Town::getPopulation() const
+{
+   return m_population;
+}
+
+const std::string &Town::getName() const
+{
+    return m_name;
+}
+
+const QPointF Town::getPos() const
+{
+    return m_pos;
+}
+
+void Town::processTick(World *)
 {
 
 }
