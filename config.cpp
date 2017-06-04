cd8b7b2bc63a6558774b1d863c7559c6a7ee8275
Brendan Hosking <brendan.hosking@gmail.com>
2017-06-04 22:03:59 +1000

    Add Resource data initialization
    There are no actual resources in the code.
    
    Create a  Config class that can have resources and their needs added
    into the loadResources function (currently hardcoded here, but could
    be changed in future). A given resource and a map of the resources
    can be accessed using getResource and getResources respectively.
    Initialise with semi dummy resources Food and Iron.

--- config.cpp
+++ config.cpp
@@ -1,12 +1,31 @@
 #include "config.h"
-#include <unordered_set>
 
 Config::Config()
 {
-
+    loadResources();
 }
 
-std::vector<Resource *> Config::loadResources()
+const std::unordered_map<std::string, Resource *> &Config::getResources() const
 {
+    return m_resources;
+}
 
+const Resource *Config::getResource(std::string name) const
+{
+    return getResources().at(name);
+}
+
+void Config::loadResources()
+{
+    std::vector <Resource *> resources;
+    // Resource Declaration
+    // Parameters are name, value, mass, volume, decay
+    resources.push_back(new Resource("Food", 10, 1, 2, 0.01));
+    resources.push_back(new Resource("Iron", 40, 1, 0.5, 0.02));
+
+    for (Resource * resource : resources) {
+        m_resources[resource->getName()] = resource;
+    }
+    // Resource Needs
+    m_resources.at("Iron")->addNeed(getResource("Food"), 3);
 }
--- config.h
+++ config.h
@@ -1,13 +1,18 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 #include "resource.h"
+#include <unordered_map>
 
 
 class Config
 {
 public:
     Config();
-    std::vector<Resource*> loadResources();
+    const std::unordered_map<std::string, Resource *> & getResources() const;
+    const Resource * getResource(std::string name) const;
+private:
+    void loadResources();
+    std::unordered_map<std::string, Resource *> m_resources;
 };
 
 #endif // CONFIG_H
