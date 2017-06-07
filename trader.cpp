2cbe968363d985e0ad0fb7b528528fd6c7cfccbc
Brendan Hosking <brendan@inspiredsoftware.com.au>
2017-06-07 15:23:37 +1000

    Add inventory on Trader
    Traders have no mechanism for moving stock from one town to another
    for fun and profit. This is a two part problem, keep track of what the
    trader is carrying and provide a means to move these resources to and
    from other entities.
    
    Implement a solution to the first problem. Add an inventory property
    to the Trader class. This involves the creation of several helper
    functions: getInventory, setInventory, adjustInventory,
    getInventoryResource, setInventoryResource and adjustInventoryResource.

--- trader.cpp
+++ trader.cpp
@@ -36,6 +36,47 @@ void Trader::setDestinationTown(const Town * destination)
     m_destination = destination;
 }
 
+const std::unordered_map<const Resource *, int> &Trader::getInventory() const
+{
+    return m_inventory;
+}
+
+void Trader::setInventory(const std::unordered_map<const Resource *, int> other)
+{
+    m_inventory = other;
+}
+
+int Trader::getInventoryResource(const Resource *resource) const
+{
+    std::unordered_map<const Resource *, int>::const_iterator got = getInventory().find(resource);
+    if (got == getInventory().end())
+    {
+        return 0;
+    }
+    else
+    {
+        return got->second;
+    }
+}
+
+void Trader::adjustInventoryResource(const Resource *resource, int change)
+{
+    setInventoryResource(resource, getInventoryResource(resource) + change);
+}
+
+void Trader::setInventoryResource(const Resource *resource, int value)
+{
+    m_inventory[resource] = value;
+}
+
+void Trader::adjustInventory(const std::unordered_map<const Resource *, int> changes)
+{
+    for (std::pair<const Resource *, int> resourceChange : changes)
+    {
+        adjustInventoryResource(resourceChange.first, resourceChange.second);
+    }
+}
+
 void Trader::processTick(World &)
 {
 
