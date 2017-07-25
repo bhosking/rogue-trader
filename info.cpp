fc6951d27a01b4fea87043bbdf73402bbd3d7f7d
Cameron Hosking <hippiemancam@gmail.com>
2017-07-25 11:40:52 +1000

    Remove case for when info is null from getValue
    getValue had code specifically for when the object was null
    
    Remove the code from Infos getValue function that pertains to specifically when the info object is null.
    This is because dereferencing a null pointer and calling its function is Undefined Behavior.
    It just so happens that for many compilers the undefined behavior results in running the code within the function however it is also within its rights to segfault (or do anything else).
    Its best that if the function is ever called on a nullptr that program crash so that instances where this can occur are removed.

--- info.cpp
+++ info.cpp
@@ -86,7 +86,7 @@ int Info::getGP() const
 
 int Info::getValue() const
 {
-    return this ? 50 + 50 * 1440 / (getAgeOfInfo() + 1440) : 0;
+    return 50 + 50 * 1440 / (getAgeOfInfo() + 1440);
 }
 
 std::string Info::getAgeOfInfoAsString() const
